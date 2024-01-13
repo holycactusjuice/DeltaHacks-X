23d6bec5a3d2878217281dbf3d431afececf08ba
Annika <annika.xu19@gmail.com>
2024-01-13 14:37:52 -0500

    Commit transcript to summary code

---
+++ transcript.py
@@ -0,0 +1,81 @@
+import pprint
+import os
+from youtube_transcript_api import YouTubeTranscriptApi
+from openai import OpenAI
+
+client = OpenAI(
+  api_key="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"
+)
+
+# test parameters (replace with user input later)
+url = 'https://www.youtube.com/watch?v=NJZ5YNrXMpE&ab_channel=oliSUNvia'
+#url = 'https://www.youtube.com/watch?v=h6fcK_fRYaI&ab_channel=Kurzgesagt%E2%80%93InaNutshell'
+wordcount = 100
+
+video_id = url.replace('https://www.youtube.com/watch?v=', '')
+transcriptjson = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', "en-GB"])
+
+
+# split transcript up into 10000-character chunks
+def chunk_transcript(transcript, chunk_size=10000):
+    chunks = [transcript[i:i+chunk_size] for i in range(0, len(transcript), chunk_size)]
+    return chunks
+
+#debug: pprint.pprint(transcriptjson)
+
+# get transcript from youtube
+def getTranscriptText(transcriptjson):
+  transcript=''
+  for item in transcriptjson:
+      clip = item['text']
+      transcript += f' {clip}' 
+
+  return transcript
+
+# get summary of transcript using openai (wordcount specified by user)
+def getSummary(transcript_chunks, wordcount):
+  summary = ""
+
+  # commands for openai
+  conversation=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "assistant", "content": f"Write a {wordcount} word summary of this video."}]
+  
+  # run it on every chunk of transcript
+  for chunk in transcript_chunks:
+      conversation.append({"role": "user", "content": chunk})
+      response = client.chat.completions.create(
+          model="gpt-3.5-turbo",
+          messages=conversation
+      )
+      summary += f" {response.choices[0].message.content}"
+
+      # Clear user input for the next chunk
+      conversation = conversation[:-1]
+
+  return summary
+
+transcriptText = getTranscriptText(transcriptjson)
+
+# keep on passing through ai until reaches specified wordcount
+# every word has ~6.5 characters on average
+while len(transcriptText)>wordcount*6.5:
+    transcript_chunks = chunk_transcript(transcriptText)  
+    transcriptText = getSummary(transcript_chunks, wordcount)
+
+# output to user interface
+print(transcriptText)
+
+"""response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "assistant", "content": "Write a 100 word summary of this video."},
+    {"role": "user", "content": getTranscriptText(transcriptjson)}
+  ]
+)
+
+summary = response.choices[0].message.content
+
+print(summary)
+"""
