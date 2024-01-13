23572f7bc4989ad493bb82e2566f1d9deb6b6c6a
Patrick <73622070+holycactusjuice@users.noreply.github.com>
2024-01-13 15:33:24 -0500

    what

---
+++ server/website/__init__.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from flask_login import LoginManager
+from pymongo import MongoClient
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+MONGODB_KEY = os.getenv('MONGODB_KEY')
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+
+
+client = MongoClient(
+    f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.iruvwvi.mongodb.net/{DB_NAME}?retryWrites=true&w=majority')
+db = client[DB_NAME]
+users = db['users']
+tracks = db['tracks']
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = os.urandom(32)
+
+    app.config['MONGO_URI'] = f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.iruvwvi.mongodb.net/{DB_NAME}?retryWrites=true&w=majority'
+
+    mongo = PyMongo()
+    mongo.init_app(app)
+
+    # configure CORS to allow requests from frontend
+    # allow cookies so that session can be used
+    CORS(app, resources={
+         r"/api/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.get(user_id)
+
+    return app
---
+++ server/website/summarizer.py
@@ -0,0 +1,20 @@
+import pprint
+import os
+from youtube_transcript_api import YouTubeTranscriptApi
+from openai import OpenAI
+
+class Summarizer:
+    api_key = 
+
+
+    """
+    jsonify(
+    {
+        "video_title": "title",
+        "video_url": "url",
+        
+    })
+    
+    data.video_title 
+    
+    """
