1c9d81513ee9d0b2e4ae65d6e87c078f731ca48b
Annika <annika.xu19@gmail.com>
2024-01-13 16:41:03 -0500

    Recovered prev changes

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from website import create_app
 
 app = create_app()
 
+app.secret_key = "DeltaHacks10"
+
 if __name__ == '__main__':
     app.run(debug=True) #rerun our code everytime we make a change
--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,11 @@
 from flask import Blueprint, render_template, request, flash
+import pymongo
+import bcrypt
 
 auth = Blueprint('auth', __name__)
+client = pymongo.MongoClient("mongodb+srv://annikaxu19:b4wE9gtG9KIvQMiG@cluster0.6puxho6.mongodb.net/")
+db = client.get_database("total_records")
+users = db.users
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -25,7 +30,15 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters', category='error')
         else:
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            user_input = {'firstname': firstName, 'email':email, 'password':hashed}
+            users.insert_one(user_input)
+
+            user_data = users.find_one({"email":email})
+            new_email = user_data["email"]
+
             flash('Account created!', category='success')
+            return render_template('signedin.html')
 
     return render_template('signup.html')
 
